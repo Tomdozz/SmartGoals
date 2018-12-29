1930ee342847a0d312919f698514e0acbc3b3e0e
Tomdozz <tom_1995@msn.com>
2018-12-30 00:38:53 +0100

    10: hover overlay function
    next fix db connection

--- main.js
+++ main.js
@@ -3,14 +3,46 @@ $(document).ready(function() {
     function() {
       getLetterData(this);
     },
-    function() {}
+    function() {
+      resetLetterData();
+    }
   );
 });
+
 function menutoggle(x) {
   x.classList.toggle("change");
   $(".menu-container").toggleClass("change");
   $(".menu-item").toggleClass("change");
 }
+
+function resetLetterData() {
+  var text = "";
+  var cat = $("#category");
+  text = "";
+  $("#category-text").text(text);
+  cat.css("width", "0%");
+}
+
 function getLetterData(x) {
-    console.log(x);
+  var text = "";
+  var cat = $("#category");
+  switch (x.id) {
+    case "s":
+      console.log("S");
+      break;
+    case "m":
+      console.log("M");
+      break;
+    case "a":
+      console.log("A");
+      break;
+    case "r":
+      console.log("R");
+      break;
+    case "t":
+      console.log("T");
+      break;
+  }
+  $("#category-text").text(text);
+  cat.css("width", "1000%");
 }
