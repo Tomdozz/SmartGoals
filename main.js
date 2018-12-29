5b3ce7fa4f6fe6a68d16b757ac7ee41b9a6d2999
Tomdozz <tom_1995@msn.com>
2018-12-29 23:37:42 +0100

    9: added section with smart goal
    added main-goal sectione with function to hover over letters
    next: find way to store date for specific users and make login

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function menutoggle(x){
-    x.classList.toggle("change");
-    $(".menu-container").toggleClass("change");
-    $(".menu-item").toggleClass("change");
+$(document).ready(function() {
+  $(".letter").hover(
+    function() {
+      getLetterData(this);
+    },
+    function() {}
+  );
+});
+function menutoggle(x) {
+  x.classList.toggle("change");
+  $(".menu-container").toggleClass("change");
+  $(".menu-item").toggleClass("change");
+}
+function getLetterData(x) {
+    console.log(x);
 }
